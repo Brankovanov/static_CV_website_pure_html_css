a69d08a435089565680819e8eae8d840b963fb60
Georgi Brankovanov <georgi.brankovanov@gmail.com>
2025-01-24 21:11:46 +0200

    JS scripts update

---
+++ scripts/constants.js
@@ -0,0 +1,150 @@
+export const constants = {
+  // Header ids
+  titleId: "title",
+  metaTagId: '[name="apple-mobile-web-app-title"]',
+  descriptionTagId: '[name="description"]',
+  logoId: "header-logo",
+
+  // Header navigation ids
+  // headerNavigationId: '[id="navigation-header-menu"]',
+  headerNavItemId: '[id="header-navigation-list"]',
+ 
+  
+  // Side navigation ids 
+  sideNavigationId: '[id="navigation-side-menu"]',
+  navItemId: '[id="navigation-list"]',
+  languageSelectId: '[id="language-select"]',
+  footerLanguageSelectId: '[id="footer-language-select"]',
+
+  // // Carousel ids
+  // carouselControlsId: '[id="carousel-controls"]',
+  // slidesWrapperId: '[id="slides-wrapper"]',
+
+  // About us ids
+  aboutMeSectionId: '[id="bio"]',
+  aboutMeImageId: '[id="about-me-image"]',
+  aboutMeTitleId: '[id="about-me-title"]',
+  aboutMeBodyId: '[id="about-me-body"]',
+  aboutMePersonNameId: '[id="about-me-name"]',
+  aboutMePersonAgeId: '[id="about-me-age"]',
+  aboutMeNavigationId: "#bio",
+
+  //WorkExperience ids
+  experienceSectionId: '[id="work-experience"]',
+  experienceListTitleId: '[id="experiences-title"]',
+  experienceContainerId: '[id="experience-wrapper"]',
+  experienceTitleId: '[id="experience-title"]',
+  experienceContentId: '[id="experience-content"]',
+  experienceDetailsId: '[id="experience-details"]',
+  workExperienceNavigationId: "#work-experience",
+
+  //Education ids
+  educationsSectionId: '[id="education"]',
+  educationsTitleId: '[id="educations-title"]',
+  educationsContentWrapperId: '[id="education-list"]',
+  educationTitleId: '[id="education-title"]',
+  educationDescriptionId: '[id="education-description"]',
+  educationAttributes: '[id="education-attributes"]',
+  educationNavigationId: "#education",
+
+  //Courses and certificates ids
+  coursesAndCertificatesNavigationId: "#courses-and-certificates",
+  coursesAndCertificatesSectionId: '[id="courses-and-certificates-wrapper"]',
+  coursesAndCertificatesTitleId: '[id="courses-and-certificates-title"]',
+  coursesAndCertificatesSlidesWrapper: '[id="courses-and-certificates-slides-wrapper"]',
+  coursesAndCertificatesFor: 'for',
+  coursesAndCertificatesChecked: 'checked',
+
+  //Our team ids
+
+  
+  teamMemberImageId: '[id="team-member-image"]',
+  teamMemberNameId: '[id="team-member-name"]',
+  teamMemberPositionId: '[id="team-member-position"]',
+  teamBioIdentifier: 'data-bio',
+  
+  //Projects ids
+  projectsNavigationId: "#projects",
+
+  //Testimonials
+  testimonialsSectionId: '[id="testimonials-wrapper"]',
+  testimonialsTitleId: '[id="testimonials-title"]',
+  testimonialsWrapperId: '[id="testimonials-wrapper"]',
+  testimonialContentId: '[id="testimonial-content"]',
+  testimonialCredentialsId: '[id="testimonial-credentials"]',
+  testimonialDateId: '[id="testimonial-date"]',
+
+  //Skills ids 
+  skillsNavigationId: "#skills",
+  //Languages ids
+  languagesNavigationId: "#languages",
+
+  //Contacts 
+  contactsNavigationId: "#contacts",
+  contactsSectionId: '[id="contact-section-container"]',
+  contactsSectionTitleId: '[id="contacts-section-title"]',
+  inputName: '[id="name-input"]',
+  inputEmail: '[id="email-input"]',
+  inputPhone: '[id="phone-input"]',
+  inputSelect: '[id="select-input"]',
+  inputTextArea: '[id="text-area-input"]',
+  phoneNumber: '[id="phone-number"]',
+  phoneLinkPrefix: 'tel:',
+  emailAddress: '[id="email-address"]',
+  mailLinkPrefix: 'mailto:',
+  address: '[id="address"]',
+  sendButtonId: '[id="submit-button"]',
+
+  //Footer
+  footerContentId: '[id="footer-wrapper"]',
+
+  //Modals
+  modalLabelDataAttribute: '[data-modal-label="modalLabel"]',
+  modalImageDataAttribute: '[data-modal-label="modalImage"]',
+  modalBodyDataAttribute: '[data-modal-label="modalBody"]',
+  contentLabelDataAttribute: '[data-content-label="contentLabel"]',
+  contentImageDataAttribute: '[data-content-image="contentImage"]',
+  contentBodyDataAttribute: '[data-content-body="contentBody"]',
+
+  //Spinner
+  spinnerIdentifier: '[id="spinner"]',
+
+  //Side navigation
+  sideNavigationIdentifier: '[id="navigation-side-menu"]',
+
+  //Bootstrap data attribute
+  bsDataTargetAttribute: 'data-bs-target',
+  bsAriaControl: 'aria-controls',
+  bsArialLabeledBy: 'aria-labelledby',
+  bsPaneIdentifier: '-pane',
+  bsTabIdentifier: '-tab',
+
+  //Classes 
+  active: 'active',
+  show: 'show',
+  displayNone: 'd-none',
+  displayFlex: 'd-flex',
+  noScroll: 'no-scroll',
+  button: 'button',
+
+  //Language
+  bulgarianLanguage: 'bg',
+  englishLanguage: 'en',
+  germanLanguage: 'de',
+
+  // Cookies
+  cookieTypes: {
+    language: 'language'
+  },
+
+  // Home
+  homeUrl: '/',
+  contentAssetOrigin: 'assets/content/',
+  contentJsonFile: '-content.json',
+
+  //Error
+  httpError: 'HTTP error! Status: ',
+  fetchingError: 'Error fetching JSON:',
+
+  yearCreated: 2024
+}
